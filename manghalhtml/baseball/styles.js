317bac3fa3554173659909511a42b17036f671df
Leeseungmin0912 <leesn0913@naver.com>
2024-11-16 11:17:09 +0900

    main

--- manghalhtml/baseball/styles.js
+++ manghalhtml/baseball/styles.js
@@ -1,4 +1,13 @@
-
 function showAlert(team) {
     alert(team + " 홈페이지로 이동합니다.");
+}
+
+function showModal(teamName, teamInfo) {
+    document.getElementById("modalTitle").innerText = teamName;
+    document.getElementById("modalContent").innerText = teamInfo;
+    document.getElementById("teamModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("teamModal").style.display = "none";
 }
