84e12f4899e3811ba8b87aa3abe8d357312fbe3d
Leeseungmin0912 <leesn0913@naver.com>
2024-12-02 17:00:59 +0900

    main

---
+++ 003/styles.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    // 마우스오버 메뉴
+    $(".navi > li")
+      .mouseover(function () {
+        $(this).find(".subMenu").stop().slideDown(300);
+      })
+      .mouseout(function () {
+        $(this).find(".subMenu").stop().slideUp(300);
+      });
+  
+    // 이미지슬라이드 페이드 인 페이드 아웃
+    $(".imgSlide a:gt(0)").hide();
+  
+    setInterval(function () {
+      $(".imgSlide a:first-child")
+        .fadeOut()
+        .next("a")
+        .fadeIn()
+        .end()
+        .appendTo(".imgSlide");
+    }, 3000);
+  
+    // **  탭 메뉴 공지사항 갤러리  ** //
+    $(function () {
+      $(".tabMenu>li>a").click(function () {
+        $(this).parent().addClass("active").siblings().removeClass("active");
+      });
+    });
+  
+    // 레이어 팝업
+    $(".notice li:first").click(function () {
+      $("#modalWrap").addClass("active");
+    });
+    $(".btn").click(function () {
+      $("#modalWrap").removeClass("active");
+    });
+  });
