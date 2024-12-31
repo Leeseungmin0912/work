6c120e3ecde970d6e00b4eae87d6019ff66b7cad
Leeseungmin0912 <leesn0913@naver.com>
2024-12-31 20:56:21 +0900

    main

---
+++ 01/10-제공파일/script.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function(){
+    // ******  상단메뉴  ****** //
+    $('.navi>li').mouseover(function(){
+        $(this).find('.subMenu').stop().slideDown(500);
+    }).mouseout(function(){
+        $(this).find('.subMenu').stop().slideUp(500);
+    });
+  
+    // ******  이미지 슬라이드  ****** //
+    $('.imgSlide a:gt(0)').hide();
+  
+    setInterval(function(){
+      $('.imgSlide a:first-child').fadeOut()
+         .next('a').fadeIn()
+         .end().appendTo('.imgSlide');},3000);
+  
+    // ******  탭 메뉴 공지사항 갤러리  ****** //
+    $(function(){
+    $('.tabMenu>li>a').click(function(){
+        $(this).parent().addClass("active").siblings().removeClass("active");
+      });
+    });
+  
+     // ******  Modal 팝업  ****** //
+  
+    $(".notice li:first").click(function(){
+         $("#modalWrap").addClass("active");
+    });
+      $(".btn").click(function(){
+      $("#modalWrap").removeClass("active");
+    });
+  
+  });
